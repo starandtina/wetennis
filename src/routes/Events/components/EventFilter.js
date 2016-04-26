d79cb6c18d8814aa8042a321ef5422d4652f96a6
Long Chen <Long.Chen@activenetwork.com>
2016-04-26 20:35:34 +0800

    cache work.

--- src/routes/Events/components/EventFilter.js
+++ src/routes/Events/components/EventFilter.js
@@ -1,16 +1,20 @@
 import React from "react";
+import cs from "./EventFilter.scss";
 
 export default class EventFilter extends React.Component {
   render() {
     return (
-      <div>
+      <div className={cs.container}>
         <div
+          className={cs["item-active"]}
           onClick={this.eventFilter.bind(this, "ALL")}
         >所有赛事</div>
         <div
+          className={cs.item}
           onClick={this.eventFilter.bind(this, "RECOMMENDATION")}
         >推荐赛事</div>
         <div
+          className={cs.item}
           onClick={this.eventFilter.bind(this, "ME")}
         >我的赛事</div>
       </div>
--- src/routes/Events/components/EventList.js
+++ src/routes/Events/components/EventList.js
@@ -1,27 +1,30 @@
 import React from "react";
 import {Link} from "react-router";
+import classes from "./EventList.scss";
 
 export default class EventList extends React.Component {
   render() {
     const {data} = this.props;
     return (
-      <div className="eventList">
+      <div className={classes.eventList}>
         {data.map((item, index) => {
           return (
             <Link
               key={index}
-              className="eventList-item"
+              className={classes["eventList-item"]}
               to={`/events/${item.id}`}
             > 
-              <div className="eventList-left">
-                <img src={item.thumb} className="thumb" alt="" />
-                <div className="type">{item.type}</div>
+              <div className={classes["eventList-left"]}>
+                <img src={item.thumb} className={classes["eventList-thumb"]} alt="" />
+                <div className={classes["eventList-type"]}>{item.type}</div>
               </div>
 
-              <div className="eventList-right">
-                <div className="name">{item.name}</div>
-                <div className="date">{`${item.startDate} - ${item.endDate}`}</div>
-                <div className="location">{item.location}</div>
+              <div className={classes["eventList-right"]}>
+                <div className={classes["eventList-name"]}>{item.name}</div>
+                <div className={classes["eventList-date"]}>
+                  {`${item.startDate} - ${item.endDate.split('.')[2]}`}
+                </div>
+                <div className={classes["eventList-location"]}>{item.location}</div>
               </div>
             </Link>
           );
