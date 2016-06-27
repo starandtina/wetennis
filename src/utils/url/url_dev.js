7415f798438be79eac9619ca00cfb8f03aca41a4
starandtina <kandmforever@126.com>
2016-06-27 22:13:25 +0800

    feat: Add API endpoint for referee

--- src/utils/url/url_dev.js
+++ src/utils/url/url_dev.js
@@ -1,12 +1,18 @@
 const ROOT_URL = '/api'
 
+// Dashbaord
 export const signUp = `${ROOT_URL}/signup`
 export const signIn = `${ROOT_URL}/signin`
-export const fetchEventGroups = `${ROOT_URL}/eventGroups`
+
+// Event
 export const events = `${ROOT_URL}/events`
+export const fetchEventGroups = `${ROOT_URL}/eventGroups`
 export const eventFilter = `${ROOT_URL}/eventFilter`
 export const fetchRegisteredUsers = `${ROOT_URL}/registeredUsers`
 export const fetchEventDetails = `${ROOT_URL}/eventDetails`
 export const fetchEventNotices = `${ROOT_URL}/eventNotices`
 export const fetchEventSponsors = `${ROOT_URL}/eventSponsors`
 export const fetchEventComments = `${ROOT_URL}/eventComments`
+
+// Referee
+export const fetchReferee = `${ROOT_URL}/referee`
