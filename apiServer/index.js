c5a553c927b85c00b7430ca1cb0d029f73b7854a
Khalil Zhang <khalil.zhang@activenetwork.com>
2016-08-30 16:55:30 +0800

    chore: Upgrade koa-router to latest

--- apiServer/index.js
+++ apiServer/index.js
@@ -1,39 +1,22 @@
 const Koa = require("koa");
 const Router = require("koa-router");
-const body = require("koa-better-body");
-const cors = require("koa-cors");
 const app = new Koa();
 
 const router = Router({
   prefix: "/api"
 });
 
-// app.use(body());
-// app.use(cors({
-//   credentials: true
-// }));
-
-
-// router.post('/:method', function (rep, res) {
-//   var fs = require('fs')
-//   var path = require('path')
-//   var file = path.resolve(__dirname, 'JSON', rep.params.method + '.json')
-
-//   var data = fs.readFileSync(file, 'utf8')
-
-//   console.log('Reading JSON file from ' + file + ' for ' + rep.path);
-//   rep.body = JSON.parse(data)
-// });
-
-router.post('/', function (rep, res) {
+router.post('/', function (ctx, next) {
   var fs = require('fs')
   var path = require('path')
-  var file = path.resolve(__dirname, 'JSON', rep.query.method + '.json')
+  var file = path.resolve(__dirname, 'JSON', ctx.query.method + '.json')
 
   var data = fs.readFileSync(file, 'utf8')
 
-  console.log('Reading JSON file from ' + file + ' for ' + rep.path);
-  rep.body = JSON.parse(data)
+  console.log('Reading JSON file from ' + file + ' for ' + ctx.path);
+  ctx.body = JSON.parse(data)
+
+  return next()
 });
 
 
