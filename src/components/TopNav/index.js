eb141c86e38a8fe32022617838b1a5dca448f82e
Long Chen <Long.Chen@activenetwork.com>
2016-10-18 22:39:39 +0800

    fix: use document.documentElement.offsetTop instead document.body.offsetTop

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -24,7 +24,7 @@ export default class TopNav extends React.Component {
   }
 
   transparentControll = () => {
-    const top = document.body.scrollTop;
+    const top = document.documentElement.scrollTop;
     const { transparent } = this.state;
     let newTransparent = transparent;
     if (top > 10) {
