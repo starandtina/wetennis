81d33303f9432a94951739977de12672e31b0955
starandtina <kandmforever@126.com>
2016-10-30 21:14:49 +0800

    chore

--- src/routes/Program/components/Match.js
+++ src/routes/Program/components/Match.js
@@ -4,17 +4,17 @@ import cs from './Program.scss'
 
 export default class Match extends Component {
   render() {
-    const { match, players, courtId, matchId, index } = this.props
-    const [playerAId, playerBId] = match.players
+    const { match: { players: matchPlayers = [], name }, players, courtId, matchId, index } = this.props
+    const [playerAId, playerBId] = matchPlayers
     const playerAName = playerBId && players[playerAId].name
     const playerBName = playerBId && players[playerBId].name
 
-    return <div 
+    return <div
         className={cs.match}
         data-court-id={courtId}
         data-match-id={matchId}>
       {index >= 0 ? `第${index+1}场` : ''}
-      <p>{match.name}</p>
+      <p>{name}</p>
       <p>{playerAName} VS {playerBName}</p>
     </div>
   }
