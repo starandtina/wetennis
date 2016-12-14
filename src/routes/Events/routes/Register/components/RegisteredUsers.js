5e476e4c9a04253793fa00405b7298d267c62bd3
starandtina <kandmforever@126.com>
2016-12-14 20:52:32 +0800

    chore: Update styles

--- src/routes/Events/routes/Register/components/RegisteredUsers.js
+++ src/routes/Events/routes/Register/components/RegisteredUsers.js
@@ -11,10 +11,10 @@ export const RegisterView = ( { registeredUsers } ) => {
           <div className='col-xs-2'>
             <img style={{width: "32px", height: "32px"}} src={user.thumbImgUrl[0]} alt="..." className='img-circle' />
           </div>
-          <div className='col-xs-7'>
+          <div className='col-xs-6'>
             <div className={classes.fontcolor}>{user.name.join(' ')}</div>
           </div>
-          <div className='col-xs-3'>
+          <div className='col-xs-4'>
             <div className={classes.right}>{user.registerDatestr}</div>
           </div>
         </div>
