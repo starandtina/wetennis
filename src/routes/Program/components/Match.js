41085f1115d9d5d1ad6d228c4e5b6aa45ab5fc6e
starandtina <kandmforever@126.com>
2017-02-16 23:10:23 +0800

    fix: typo

--- src/routes/Program/components/Match.js
+++ src/routes/Program/components/Match.js
@@ -6,7 +6,7 @@ export default class Match extends Component {
   render() {
     const { match: { players: matchPlayers = [], name }, players, courtId, matchId, index } = this.props
     const [playerAId, playerBId] = matchPlayers
-    const playerAName = playerBId && players[playerAId].name
+    const playerAName = playerAId && players[playerAId].name
     const playerBName = playerBId && players[playerBId].name
 
     return <div
