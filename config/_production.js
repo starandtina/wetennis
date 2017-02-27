b100b60bf37522c7616c2ac34488bdc6c6406a81
starandtina <kandmforever@126.com>
2017-02-27 22:17:30 +0800

    chore: Remove source map for production env

--- config/_production.js
+++ config/_production.js
@@ -2,7 +2,9 @@
 export default () => ({
   compiler_fail_on_warning : false,
   compiler_hash_type       : 'chunkhash:5',
-  compiler_devtool         : null,
+  // If you load a page without dev tools opened, there is no http request for source maps.
+  // http://stackoverflow.com/questions/23848364/performance-impact-of-using-css-javascript-source-maps-in-production
+  // compiler_devtool         : null,
   compiler_stats           : {
     chunks : true,
     chunkModules : true,
