883fcee39f258fe8e8422cabe9006c90a29b9c70
starandtina <kandmforever@126.com>
2017-03-03 22:32:07 +0800

    chore: Group registered members

--- src/routes/Events/routes/Details/routes/RegisteredMembers/components/RegisteredMembers.js
+++ src/routes/Events/routes/Details/routes/RegisteredMembers/components/RegisteredMembers.js
@@ -10,12 +10,24 @@ export default class RegisteredMembers extends  PureComponent  {
     fetchRegisteredMembers(eventId)
   }
   render() {
-    const { registeredMembers } = this.props
+    const { registeredMembers = [] } = this.props
+    const groupedRegisteredMembers = registeredMembers.reduce((r, m) => {
+      const key = `${m.groupName} - ${m.itemName}`
+      
+      r[key] = r[key] ? [...r[key], m] : []
+      
+      return r
+    }, {})
 
     return (
       <div className='u-has-nav container'>
         <NavBack routes={this.props.routes} title='赛事报名列表' transparent />
-        <RegisteredUsers registeredUsers={registeredMembers} />
+        {Object.keys(groupedRegisteredMembers).map(k => (
+          <div key={k}>
+            <div className='well'>{k}</div>
+            <RegisteredUsers registeredUsers={groupedRegisteredMembers[k]} />
+          </div>
+        ))}
       </div>
     )
   }
